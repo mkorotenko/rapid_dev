ae04f55091c28636b406aeb8cfb9d6fcff863419
Maxim Korotenko <m.v.korotenko@gmail.com>
2018-08-25 18:49:14 +0300

    Refactoring

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();  
 var server = require('http').createServer(app); 
 var io = require('socket.io')(server);
-var events = require('events');
+//var events = require('events');
 
 var debugMode = false;
 
@@ -13,21 +13,18 @@ app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/dist/index.html');
 });
 
-var eventEmitter = new events.EventEmitter();
+//var eventEmitter = new events.EventEmitter();
 //when a client connects, do this
-io.on('connection', function(client) {  
+io.on('connection', function(client) {
+
     console.log('Socket: client connected');
     client.on('debugMode', function(data) { //get light switch status from client
        console.log('debugMode:', data); //turn LED on or off, for now we will just show it in console.log
        debugMode = data;
-       eventEmitter.on('newData', function() {
+       //eventEmitter.on('newData', function() {
         client.emit('newData', debugMode);
-       });
+       //});
    });
-
-   client.on('sendToSerial', function(data) { //get light switch status from client
-        console.log('sendToSerial:', data); //turn LED on or off, for now we will just show it in console.log
-    });
 
 });
 
