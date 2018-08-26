2fcc56f376aef962d735dae8ad2cc827959e7c0b
Maxim Korotenko <m.v.korotenko@gmail.com>
2018-08-27 00:12:49 +0300

    App module #1

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,10 +1,29 @@
+import * as THREE from "../lib/three.module.js";
+
+const stats = new Stats();
 class Application {
     constructor() {
-        console.info('Application running 2');
+        console.info('Application running 3', THREE);
+        this.attachContainer();
     }
 
     unload() {
-        console.info('Application unload 2');
+        let container;
+        if (container = document.getElementById('webgl'))
+            container.remove();
+        console.info('Application unload 3');
+    }
+
+    attachContainer() {
+        let container;
+        if (container = document.getElementById('webgl'))
+            container.remove();
+
+        container = document.createElement( 'div' );
+        container.id='webgl';
+        container.appendChild( stats.dom );
+        document.body.appendChild( container );
+        
     }
 }
 
