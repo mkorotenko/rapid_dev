40edb1b2eb65b0d8fa6ad7e7356416293c9f0e4f
Maxim Korotenko <m.v.korotenko@gmail.com>
2018-09-11 10:22:03 +0300

    Added sphere

--- dist/scripts/models/poi.js
+++ dist/scripts/models/poi.js
@@ -1,35 +1,34 @@
 import * as THREE from "../../lib/three.module.js";
 
+function getBoundingBoxes(group) {
+    let result = group.children.map(m => m.geometry.boundingBox);
+
+    return result;
+}
+
 export default class PoiLoader {
 
     constructor() {
 
-        // var pinkMat = new THREE.MeshPhongMaterial({
-        //     color: new THREE.Color("rgb(206,35,213)"),
-        //     emissive: new THREE.Color("rgb(55,28,34)"),
-        //     specular: new THREE.Color("rgb(255,155,155)"),
-        //     shininess: 3,
-        //     flatShading: THREE.FlatShading,
-        //     transparent: true,
-        //     opacity: 1
-        // });
         var group = new THREE.Group();
 
-        // var mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
-        // var geo = new THREE.BoxGeometry( 20, 20, 20 );
-        // //new THREE.IcosahedronGeometry(25, 5)
-        // group.add(new THREE.Mesh(geo, mat));
-
-        var geometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
-
-        var wireframe = new THREE.WireframeGeometry( geometry );
-        
+        var wireframe = new THREE.WireframeGeometry( new THREE.BoxBufferGeometry( 20, 20, 20 ) );
         var line = new THREE.LineSegments( wireframe );
         line.material.depthTest = false;
         line.material.opacity = 0.25;
         line.material.transparent = true;
+        line.geometry.computeBoundingBox();
         group.add( line );
-        console.info('line', line);
+
+        var wf = new THREE.WireframeGeometry( new THREE.SphereBufferGeometry( 2, 10, 10 ) );
+        var sphere = new THREE.LineSegments( wf );
+        sphere.material.depthTest = false;
+        sphere.material.opacity = 0.25;
+        sphere.material.transparent = true;
+        sphere.geometry.computeBoundingBox();
+        group.add( sphere );
+
+        console.info('boxes', getBoundingBoxes(group));
 
         this.mesh = group;
     }
